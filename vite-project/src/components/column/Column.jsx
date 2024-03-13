315a3b7cae81861fc2b871cf4ec128f1e2343e67
Карпова Юлия <am_julia@mail.ru>
2024-03-13 16:31:37 +0600

    из массива формируем карты и колонки

---
+++ vite-project/src/components/column/Column.jsx
@@ -0,0 +1,12 @@
+import Card, { allCards } from "../card/Card";
+
+
+function Column({name}) {
+     return <div className="main__column column">
+     <div className="column__title">
+         <p>{name}</p>
+     </div>
+     <Card allCards = {allCards.filter((card) => card.columnTitle === name)} />
+ </div>
+}
+export default Column;
--- vite-project/src/components/popups/PopNewCard.jsx
+++ vite-project/src/components/popups/PopNewCard.jsx
@@ -8,7 +8,7 @@ function PopNewCard() {
                     <form className="pop-new-card__form form-new" id="formNewCard" action="#">
                         <div className="form-new__block">
                             <label htmlFor="formTitle" className="subttl">Название задачи</label>
-                            <input className="form-new__input" type="text" name="name" id="formTitle" placeholder="Введите название задачи..." autofocus/>
+                            <input className="form-new__input" type="text" name="name" id="formTitle" placeholder="Введите название задачи..." autoFocus/>
                         </div>
                         <div className="form-new__block">
                             <label htmlFor="textArea" className="subttl">Описание задачи</label>
