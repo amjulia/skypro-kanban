87ed8c86a74ba047c102fc25506cdb58b23661ef
Карпова Юлия <am_julia@mail.ru>
2024-03-21 10:24:51 +0600

    убрала ссылку, сделала форматирование

--- vite-project/src/components/header/Header.jsx
+++ vite-project/src/components/header/Header.jsx
@@ -1,36 +1,53 @@
 import { useState } from "react";
 
-
-function Header({onCardAdd}) {
-    const [isOpen, setIsOpen] = useState(false);
+function Header({ onCardAdd }) {
+  const [isOpen, setIsOpen] = useState(false);
   const ToggleDropdown = () => setIsOpen((prevState) => !prevState);
 
-    return <div className="container">
-    <div className="header__block">
+  return (
+    <div className="container">
+      <div className="header__block">
         <div className="header__logo _show _light">
-            <a href="" target="_self"><img src="images/logo.png" alt="logo"/></a>
+          <a href="" target="_self">
+            <img src="images/logo.png" alt="logo" />
+          </a>
         </div>
         <div className="header__logo _dark">
-            <a href="" target="_self"><img src="images/logo_dark.png" alt="logo"/></a>
+          <a href="" target="_self">
+            <img src="images/logo_dark.png" alt="logo" />
+          </a>
         </div>
         <nav className="header__nav">
-            <button className="header__btn-main-new _hover01" id="btnMainNew" onClick={onCardAdd}>Создать новую задачу</button>
-            <a href="#user-set-target" className="header__user _hover02" onClick={ToggleDropdown} >Ivan Ivanov</a>
-            {isOpen && (<div className="header__pop-user-set pop-user-set" id="user-set-target">
-                
-                <p className="pop-user-set__name">Ivan Ivanov</p>
-                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                <div className="pop-user-set__theme">
-                    <p>Темная тема</p>
-                    <input type="checkbox" className="checkbox" name="checkbox"/>
-                </div>
-                <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
-            </div>)}
-            
-        </nav>					
+          <button
+            className="header__btn-main-new _hover01"
+            id="btnMainNew"
+            onClick={onCardAdd}
+          >
+            Создать новую задачу
+          </button>
+          <a onClick={ToggleDropdown} className="header__user _hover02">
+            Ivan Ivanov
+          </a>
+          {isOpen && (
+            <div
+              className="header__pop-user-set pop-user-set"
+              id="user-set-target"
+            >
+              <p className="pop-user-set__name">Ivan Ivanov</p>
+              <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+              <div className="pop-user-set__theme">
+                <p>Темная тема</p>
+                <input type="checkbox" className="checkbox" name="checkbox" />
+              </div>
+              <button type="button" className="_hover03">
+                <a href="#popExit">Выйти</a>
+              </button>
+            </div>
+          )}
+        </nav>
+      </div>
     </div>
-</div>			
-  }
-  
+  );
+}
 
-  export default Header;
+export default Header;
--- vite-project/src/components/mainContainer/MainContainer.jsx
+++ vite-project/src/components/mainContainer/MainContainer.jsx
@@ -1,29 +1,30 @@
 import Column from "../column/Column";
 const statusList = [
-    "Без статуса",
-    "Нужно сделать",
-    "В работе",
-    "Тестирование",
-    "Готово",
-  ];
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+];
 
-function MainContainer({cards}) {
-   return (
+function MainContainer({ cards }) {
+  return (
     <div className="container">
-        <div className="main__block">
-           <div className="main__content">
-                {statusList.map((status) => {
-                return <Column
+      <div className="main__block">
+        <div className="main__content">
+          {statusList.map((status) => {
+            return (
+              <Column
                 key={status}
                 title={status}
                 allCards={cards.filter((card) => card.status === status)}
-               />
-             })}
-            </div>
+              />
+            );
+          })}
         </div>
-   </div>
-   )
-
+      </div>
+    </div>
+  );
 }
 
 export default MainContainer;
